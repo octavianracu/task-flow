82e07b0a8955cd40c30462a0c87bb46ab0160298
octavianracu <octavianracu@gmail.com>
2023-11-10 00:04:18 +0200

    Big and small ship

---
+++ ship.py
@@ -0,0 +1,30 @@
+from os import system
+
+option = ""
+small_ship = 5
+big_ship = 5
+
+
+while True: 
+    system ("cls")
+    for x in range(1,11):
+        if x == big_ship:
+            print( "W", end="" )
+        elif x == big_ship + 1:
+                print("w", end="")
+        elif x == big_ship - 1:
+                print("w", end="")
+        else:
+            print( "~", end="" )
+       
+            
+    print("\n")          
+    option = input(">>>: ")
+
+    if option == "a":
+        small_ship -= 1
+    elif option == "d":
+        small_ship += 1
+    elif option == "x":
+        print("Game over")
+        break
